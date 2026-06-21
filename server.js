2a07fca4280fd7a133412d17ea0c6a68a3c64057
johnston <ethan.john.13579@gmail.com>
2026-06-21 18:48:45 +0400

    view stats

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const cors = require("cors");
 const nocache = require("nocache");
 const mcp = require("minecraft-protocol");
 const path = require("path");
+const fs = require("fs");
 const app = express();
 const port = 3000;
 
@@ -19,12 +20,68 @@ const mcsip = "127.0.0.1"
 const mcsport = 25565
 
 app.get("/api/players", (req, res) => {
-    console.log(`request to '/players' from ${req.ip}`)
+    console.log(`request to '/api/players' from ${req.ip}`)
     mcp.ping({ mcsip, mcsport }, (err, mcres) => {
         if (err) { return res.json({"error": "server down" }); }
 
         res.json(mcres);
     });
+});
+
+
+// stats
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "stats.html"));
+});
+app.get("/api/stats", (req, res) => {
+    console.log(`request to 'api/stats' from ${req.ip}`);
+
+    const serverDir = "/home/john/basmati";
+    const usercache = JSON.parse(fs.readFileSync(`${serverDir}/usercache.json`, "utf8"));
+
+    var retval = [];
+    fs.readdirSync(`${serverDir}/world/players/stats/`).forEach((f) => {
+        // username lookup
+        var uuid = f.replace(".json", "");
+        var username = "";
+
+        for (var i = 0; i < usercache.length; i++) {
+          if (usercache[i]["uuid"] == uuid) { username = usercache[i]["name"]; }
+        }
+
+        var data = JSON.parse(fs.readFileSync(`${serverDir}/world/players/stats/${f}`));
+
+        // deaths
+        var deaths = 0;
+        deaths = data["stats"]["minecraft:custom"]["minecraft:deaths"];
+        if (!deaths) deaths = 0;
+
+        // playtime
+        var playtime = 0;
+        playtime = data["stats"]["minecraft:custom"]["minecraft:play_time"] / 20 / 60 / 60;
+        playtime = Math.round(playtime * 10) / 10;
+
+        // distance travelled
+        var distance = 0;
+        distance = data["stats"]["minecraft:custom"]["minecraft:walk_one_cm"] / 100;
+        distance = Math.round(distance);
+
+        var crouchedDistance = 0;
+        crouchedDistance = data["stats"]["minecraft:custom"]["minecraft:crouch_one_cm"] / 100;
+        crouchedDistance = Math.round(crouchedDistance);
+
+        // console.log(`${username}'s deaths: ${deaths}; playtime: ${playtime}; ${distance}; crouched: ${crouchedDistance}`);
+  
+        retval.push({
+            "username": username,
+            "deaths": deaths,
+            "playtime": `${playtime} hours`,
+            "distance walked": `${distance} blocks`,
+            "distanced walked while crouched": `${crouchedDistance} blocks`
+        }); 
+    });    
+
+    res.json(retval);
 });
 
 app.listen(port, "0.0.0.0", () => {
